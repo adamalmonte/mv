18a9b7f2f4dccda99bd2f5773697897679f6d638
Adam Almonte <adamgalmonte@gmail.com>
2024-09-05 14:26:14 -0400

    adjusting some spacing and a variable name

--- RoverClass.ts
+++ RoverClass.ts
@@ -39,58 +39,59 @@ export class Rover {
     move(instruction: Instruction) {
         switch(instruction) {
             case "L":
-                    if (this.direction === "N") {
-                        this.direction = "W";
-                    } else if (this.direction === "S") {
-                        this.direction = "E";
-                    } else if (this.direction === "E") {
-                        this.direction = "N";
-                    } else { // W
-                        this.direction = "S";
-                    }
+                if (this.direction === "N") {
+                    this.direction = "W";
+                } else if (this.direction === "S") {
+                    this.direction = "E";
+                } else if (this.direction === "E") {
+                    this.direction = "N";
+                } else { // W
+                    this.direction = "S";
+                }
+
                 break;
             case "R":
-                    if (this.direction === "N") {
-                        this.direction = "E";
-                    } else if (this.direction === "S") {
-                        this.direction = "W";
-                    } else if (this.direction === "E") {
-                        this.direction = "S";
-                    } else { // W
-                        this.direction = "N";
-                    }
+                if (this.direction === "N") {
+                    this.direction = "E";
+                } else if (this.direction === "S") {
+                    this.direction = "W";
+                } else if (this.direction === "E") {
+                    this.direction = "S";
+                } else { // W
+                    this.direction = "N";
+                }
 
                 break;
             case "F":
-                    if (this.direction === "N") {
-                        if (this.yPos === this.maxY) {
-                            this.lost = true;
-                            break;
-                        } else {
-                            this.yPos += 1;
-                        }
-                    } else if (this.direction === "S") {
-                        if (this.yPos === 0) {
-                            this.lost = true;
-                            break;
-                        } else {
-                            this.yPos -= 1;
-                        }
-                    } else if (this.direction === "E") {
-                        if (this.xPos === this.maxX) {
-                            this.lost = true;
-                            break;
-                        } else {
-                            this.xPos += 1;
-                        }
-                    } else { // W
-                        if (this.xPos === 0) {
-                            this.lost = true;
-                            break;
-                        } else {
-                            this.xPos -= 1;
-                        }
-                    }                
+                if (this.direction === "N") {
+                    if (this.yPos === this.maxY) {
+                        this.lost = true;
+                        break;
+                    } else {
+                        this.yPos += 1;
+                    }
+                } else if (this.direction === "S") {
+                    if (this.yPos === 0) {
+                        this.lost = true;
+                        break;
+                    } else {
+                        this.yPos -= 1;
+                    }
+                } else if (this.direction === "E") {
+                    if (this.xPos === this.maxX) {
+                        this.lost = true;
+                        break;
+                    } else {
+                        this.xPos += 1;
+                    }
+                } else { // W
+                    if (this.xPos === 0) {
+                        this.lost = true;
+                        break;
+                    } else {
+                        this.xPos -= 1;
+                    }
+                }                
 
                 break;
             default:
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Rover, RoverInput, Direction, Instruction } from "./RoverClass";
 type ProcessedInput = {
     maxX: number;
     maxY: number;
-    rovers: RoverInput[];
+    roverInputs: RoverInput[];
 }
 
 const processInput = (input: string): ProcessedInput => {
@@ -15,20 +15,27 @@ const processInput = (input: string): ProcessedInput => {
         =
         [ '4 8 ', '(2, 3, E) LFRFF ', '(0, 2, N) FFLFRFF' ]
      */
+
     // split input on newline
     const inputLines = input.split('\n');
+
     // remove first line from input, trim whitespace, split on space to get x and y
     const [maxX, maxY] = inputLines.shift().trim().split(' ');
 
-    // remaining lines in input are Rover instances and instructions.
-    // '        (2, 3, E) LFRFF'
-    // => .trim() = '(2, 3, E) LFRFF'
-    // => .split(") ") = [ '(2, 3, E', 'LFRFF' ]
-    // then remove opening parenthesis and split first element to get x, y, and direction
-    // map into array of objects:
-    // [ { xPos: 2, yPos: 3, direction: "E", instructions: [L,F,R,F,F] }, ...]
-    const rovers: RoverInput[] = inputLines.map((line) => {
+    /** remaining lines in input are Rover instances and instructions.
+        '        (2, 3, E) LFRFF'
+            => .trim() = '(2, 3, E) LFRFF'
+                => .split(") ") = [ '(2, 3, E', 'LFRFF' ]
+        
+        then remove opening parenthesis and split first element to get x, y, and direction,
+        then map into array of objects:
+        
+        => [ { xPos: 2, yPos: 3, direction: "E", instructions: [L,F,R,F,F] }, ...]
+    */
+
+    const roverInputs: RoverInput[] = inputLines.map((line) => {
         const [tempPos, tempIns] = line.trim().split(") ");
+
         const position = tempPos.substring(1, tempPos.length);
         const [xPos, yPos, direction] = position.split(", ");
 
@@ -45,7 +52,7 @@ const processInput = (input: string): ProcessedInput => {
     return {
         maxX: Number(maxX),
         maxY: Number(maxY),
-        rovers
+        roverInputs
     };
 };
 
@@ -55,7 +62,7 @@ const generateOutput = (input: string) => {
     console.log(input);
     console.log("\n");
 
-    const { maxX, maxY, rovers: roverInputs } = processInput(input);
+    const { maxX, maxY, roverInputs } = processInput(input);
 
     console.log(`RESULT:`);
     for (let input of roverInputs) {
